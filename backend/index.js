8bc3808c21d46424613c989f9a3546cc84c32b97
UTTAM GUPTA <71481012+UTTAMGUPTA2712@users.noreply.github.com>
2023-04-30 00:30:33 +0530

    Adding download function

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const app = express()
 const List = require("./model/transactions")
 const User = require("./model/user")
 const Seller = require("./model/seller")
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+//user api
 app.get('/user', async (req, res) => {
     try {
         const user = await User.find()
@@ -28,8 +28,7 @@ app.post('/user', async (req, res) => {
     }
 })
 
-
-
+//seller api
 app.get("/seller", async (req, res) => {
     try {
         const seller = await Seller.find()
@@ -49,7 +48,7 @@ app.post("/seller", async (req, res) => {
     }
 })
 
-
+//transaction api
 app.get("/transaction", async (req, res) => {
     try {
         const trans = await List.find()
@@ -98,6 +97,27 @@ app.put("/transaction/:id", async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 })
+
+app.get("/download/:user", async (req, res) => {
+    try {
+      const { user } = req.params;
+      const trans = await List.find({ user });
+      const text = trans.map((t,i) => `${i+1} | ${t.amount} | ${t.seller} | ${t.createdAt} | ${t._id}`).join("\n");
+      res.set({
+        "Content-Disposition": `attachment; filename=${user}transactions.txt`,
+        "Content-Type": "text/plain",
+      });
+  
+      res.send(text);
+  
+      console.log(trans);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+
 
 mongoose.connect("mongodb+srv://uttam_gupta:Qazwsx55@cluster0.et4nl6t.mongodb.net/?retryWrites=true&w=majority")
     .then(() => {
